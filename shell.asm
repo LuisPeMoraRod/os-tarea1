8a29929cefdece89fae5fe7575cd9251e0ca09a3
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-03-03 16:36:03 -0600

    feat(shell): add clear screen method to shell

--- shell.asm
+++ shell.asm
@@ -10,10 +10,20 @@ start:
 	mov ax, 0               ; init data registers, set ACCUMULATOR REGISTER to 0
         mov ds, ax              ; ds = DATA SEGMENT register
         mov es, ax              ; es = EXTRA SEGMENT register
+
+	call clear_screen
 	
-	mov si, hello_mssg	; point SOURCE INDEX register to success message string's address
+	mov si, intro_mssg	; point SOURCE INDEX register to success message string's address
         call print              ; print message to screen
-	jmp $
+
+shell_loop:
+	jmp shell_loop
+
+clear_screen:
+	mov ah, 0x00  		; required to set video mode
+	mov al, 0x03		; video mode as text type
+	int 0x10		; INTERRUPTION: set video mode. Used to clear screen 
+	ret
 
 print:                          ; procedure to print a string
         cld                     ; clear DIRECTION FLAG
@@ -28,6 +38,24 @@ print:                          ; procedure to print a string
 
         .return: ret            ; return from procedure
 
-hello_mssg db 'Hello from SHELL', 10, 13, 0
+; procedure to read a single sector from USB flash drive
+read_sector:
+        mov ah, 0x02            ; BIOS code to read from storage device
+        mov al, 1               ; how many sector to read
+        mov ch, 0               ; specify cilinder
+        mov dh, 0               ; specify head
+        mov dl, 0x80            ; specify HDD code
+        int 0x13                ; INTERRUPTION: read the sector from USB flash drive into memory
+        jc .error               ; if failed to read sector, jump to error procedure
+        ret                     ; return from procedure
+
+        .error:
+                mov si, error_mssg      ; point SOURCE INDEX register to error message string's address
+                call print              ; print error message
+                jmp $                   ; processor holt (infinite loop)
+
+; messages
+intro_mssg db 'Welcome to MicromundOS.', 10, 13,'Type "start" to play the game: ', 0	
+error_mssg db 'Failed to read sector from USB', 10, 13, 0	; add \n (newline) before \0
 
 times 512 - ($ - $$) db 0       ; fill trailing zeros to get exactly 512 bytes long binary file
